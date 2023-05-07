0d31e453698bc96722fa25a523225234823914d9
Saiful Alam Ovi <eovi239@gmail.com>
2023-05-07 19:43:00 +0600

    update navbar.blade.php

--- resources/views/admin/navbar.blade.php
+++ resources/views/admin/navbar.blade.php
@@ -30,6 +30,16 @@
         <li class="nav-item nav-category">
           <span class="nav-link">Navigation</span>
         </li>
+        
+        <li class="nav-item menu-items">
+          <a class="nav-link" href="{{url('/users')}}">
+            <span class="menu-icon">
+              <i class="mdi mdi-speedometer"></i>
+            </span>
+            <span class="menu-title">Deshboard</span>
+          </a>
+        </li>
+
         <li class="nav-item menu-items">
           <a class="nav-link" href="{{url('/users')}}">
             <span class="menu-icon">
